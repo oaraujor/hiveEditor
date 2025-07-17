940b7fb3783158811d2e32ff5ce387dc18f3762a
Octavio Araujo Rosales <oaraujor_@hotmail.com>
2025-07-16 22:43:00 -0600

    init/initial commit, created makefile and project layout

---
+++ include/utils.h
@@ -0,0 +1,13 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdio.h>
+
+void new_project(const char*);
+
+void new_project(const char* message){
+
+	printf("%s", message);
+}
+
+#endif
---
+++ src/hiveEditor.c
@@ -0,0 +1,10 @@
+#include "utils.h"
+
+int main(){
+
+	const char * greeting = "Hello this my new project called hiveEditor\n";
+
+	new_project(greeting);
+
+	return 0;
+}
