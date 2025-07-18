e720a2272cbc24e556a951f6c8a89caa81d9f8cc
Octavio Araujo Rosales <oaraujor_@hotmail.com>
2025-07-18 00:31:31 -0600

    fix/ end line character

--- src/hiveEditor.c
+++ src/hiveEditor.c
@@ -19,31 +19,32 @@ int main()
 	WINDOW *edit_win = newwin(win_height, win_width, 1, 1);
 	
 	char buffer[BUF_LINES][BUF_COLS] = {{0}};
+	int line_len[BUF_LINES] = {0};
 	int cur_line = 0, cur_col = 0;
 	int running = true;
 	int i, ch;
 
 	while (running)
 	{
-		clear();                    // Clear entire stdscr (main screen)
-		box(stdscr, 0, 0);          // Draw a border around stdscr
-		// Status bar at the bottom: show editor name, quit hint, and cursor position
+		//werase(edit_win);
+
+		clear();
+		box(stdscr, 0, 0);
+		werase(edit_win);
 		mvprintw(LINES-1, 2, " hiveEditor | Ctrl+Q to quit | Cursor: (%d,%d) ", cur_line + 1, cur_col + 1);
-        	// --- Draw Buffer in Editing Window ---
-		werase(edit_win);           // Clear the editing window
+
+
 		i = 0;
 		for (i = 0; i < BUF_LINES; i++)
 		{
-			// Print each line of the buffer into the edit window
-			mvwprintw(edit_win, i, 0, "%s", buffer[i]);
+			mvwaddnstr(edit_win, i, 0, buffer[i], line_len[i]);
 		}
 
-		// Put the cursor at the correct place in the edit window
 		wmove(edit_win, cur_line, cur_col);
-		wrefresh(edit_win);         // Refresh edit window to show changes
-		refresh();                  // Refresh stdscr (border/status)
-		// --- Handle User Input ---
-		ch = wgetch(edit_win);  // Wait for keypress in edit window
+		wrefresh(edit_win);
+		refresh();
+
+		ch = wgetch(edit_win);
 
 		switch (ch)
 		{
@@ -51,49 +52,55 @@ int main()
 				running = false;
 				break;
 
-			case KEY_UP:
-				// Move cursor up a line if not at the top
+			case KEY_UP: // Move cursor up a line if not at the top
 				if (cur_line > 0)
 				{
 					cur_line--;
+					if (cur_col > line_len[cur_line])
+					{
+						cur_col = line_len[cur_line];
+					}
 				}
 				break;
 
-			case KEY_DOWN:
-			// Move cursor down a line if not at the bottom
+			case KEY_DOWN: // Move cursor down a line if not at the bottom
 				if (cur_line < BUF_LINES-1)
 				{
 					cur_line++;
+					if (cur_col > line_len[cur_line])
+					{
+						cur_col = line_len[cur_line];
+					}
 				}
 				break;
 
-			case KEY_LEFT:
-				// Move cursor left if not at the start of line
+			case KEY_LEFT: // Move cursor left if not at the start of line
 				if (cur_col > 0)
 				{
 					cur_col--;
 				}
 				break;
-			case KEY_RIGHT:
-				// Move cursor right if not at end of buffer
-				if (cur_col < BUF_COLS-1)
+
+			case KEY_RIGHT: // Move cursor right if not at end of buffer
+				if (cur_col < line_len[cur_line])
 				{
 					cur_col++;
 				}
 				break;
+
 			case KEY_BACKSPACE:
 			case 127:
-			case 8:
-				// Handle backspace: remove character before cursor
+			case 8: // Handle backspace: remove character before cursor
 				if (cur_col > 0)
 				{
-					// Shift text left to fill the gap
-					memmove(&buffer[cur_line][cur_col - 1], &buffer[cur_line][cur_col], BUF_COLS - cur_col);
+					memmove(&buffer[cur_line][cur_col - 1], &buffer[cur_line][cur_col], line_len[cur_line] - cur_col + 1);
 					cur_col--;
+					line_len[cur_line]--;
+					buffer[cur_line][line_len[cur_line]] = '\0';
 				}
 				break;
-			case '\n':
-				// Handle Enter: move to start of next line
+
+			case '\n': // Handle Enter: move to start of next line
 				if (cur_line < BUF_LINES - 1)
 				{
 					cur_line++;
@@ -104,20 +111,23 @@ int main()
 				// Insert printable ASCII character
 				if (ch >= 32 && ch < 127)
 				{
-					if (cur_col < BUF_COLS - 1)
+					if (line_len[cur_line] < BUF_COLS - 1)
 					{
 						// Shift text right to make room for new character
-						memmove(&buffer[cur_line][cur_col + 1], &buffer[cur_line][cur_col], BUF_COLS - cur_col - 1);
+						memmove(&buffer[cur_line][cur_col + 1], &buffer[cur_line][cur_col], line_len[cur_line] - cur_col + 1);
 						buffer[cur_line][cur_col] = ch; // Insert character
+						line_len[cur_line]++;
 						cur_col++;
+						buffer[cur_line][line_len[cur_line]] = '\0';
 					}
 				}
 				break;
 		}
+
 	}
 	// === Cleanup ncurses resources and exit ===
-	delwin(edit_win);    // Delete editing window
-	endwin();            // End ncurses mode, restore normal terminal
+	delwin(edit_win);
+	endwin();
 	
 	return 0;
 }
